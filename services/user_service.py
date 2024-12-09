0181d38f8255b31c01158f229efc5237f4ab6d84
omarchii <omar.chi@anahuacmayab.edu.mx>
2024-12-08 22:15:28 -0600

    pruebas conexion

--- services/user_service.py
+++ services/user_service.py
@@ -1,17 +1,18 @@
 from extensions import db
+from sqlalchemy.sql import text
 
 def create_user_role(user, password, role):
     try:
         # Crear el rol
-        db.session.execute(f"CREATE ROLE {role} WITH LOGIN PASSWORD '{password}';")
+        db.session.execute(text(f"CREATE ROLE {role} WITH LOGIN PASSWORD '{password}';"))
         
         # Asignar privilegios
-        db.session.execute(f"GRANT CONNECT ON DATABASE appdb TO {role};")
-        db.session.execute(f"GRANT USAGE ON SCHEMA public TO {role};")
-        db.session.execute(f"GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO {role};")
+        db.session.execute(text(f"GRANT CONNECT ON DATABASE appdb TO {role};"))
+        db.session.execute(text(f"GRANT USAGE ON SCHEMA public TO {role};"))
+        db.session.execute(text(f"GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO {role};"))
         
         # Crear usuario con rol
-        db.session.execute(f"CREATE USER {user} WITH PASSWORD '{password}' IN ROLE {role};")
+        db.session.execute(text(f"CREATE USER {user} WITH PASSWORD '{password}' IN ROLE {role};"))
         
         db.session.commit()
         return {"message": f"User '{user}' with role '{role}' created successfully."}
